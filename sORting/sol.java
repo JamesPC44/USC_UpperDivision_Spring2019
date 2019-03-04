f90c9c8143ba717328cc1ab86cdd9d7432103a6e
Matthew Clapp <clappm@users.noreply.github.com>
2019-03-03 23:20:36 -0500

    Update sol.java

--- sORting/sol.java
+++ sORting/sol.java
@@ -1,8 +1,4 @@
-import java.io.*;
 import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
 
 public class Solution {
 
@@ -14,35 +10,24 @@ public class Solution {
       arr[n] = in.nextInt();
     }
     in.close();
-    int mask = 0;
     int res = 0;
-    while (!qsorted(arr, (1 << mask) - 1)) {
-      mask++;
-    }
-    while (mask >= 0) {
-      if (!qsorted(arr, (1 << mask) - 1)) {
-        applymask(arr, 1 << mask);
-        res |= 1 << mask;
+    for (int mask = 1 << 30; mask > 0; mask >>= 1) {
+      if (!qsorted(arr, mask - 1)) {
+        for (int i = 0; i < arr.length; i++) {
+          arr[i] |= mask;
+        }
+        res |= mask;
       }
-      mask--;
     }
     System.out.println(res);
   }
 
-  public static boolean qsorted(int[] arr, int m) {
-    int max = 0;
-    for (int i = 0; i < arr.length; i++) {
-      if ((arr[i] | m) < max) {
+  public static boolean qsorted(int[] arr, int mask) {
+    for (int i = 0; i < arr.length - 1; i++) {
+      if ((arr[i] | mask) > (arr[i + 1] | mask)) {
         return false;
       }
-      max = arr[i] | m;
     }
     return true;
-  }
-
-  public static void applymask(int[] arr, int m) {
-    for (int i = 0; i < arr.length; i++) {
-      arr[i] |= m;
-    }
   }
 }
